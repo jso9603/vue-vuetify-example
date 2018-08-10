0cba0e5e35034667b58698e950de939933c8e5cf
Jisoo Park <jisoo.park@corelinesoft.com>
2018-08-10 16:23:31 +0900

    Update button vuetify component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -417,6 +417,254 @@
       <v-btn flat disabled>Disabled</v-btn>
       <v-btn flat large color="error">Error</v-btn>
     </div>
+
+    <div>
+      <v-btn small color="primary">Primary</v-btn>
+      <v-btn color="error">Error</v-btn>
+      <v-btn large>Normal</v-btn>
+      <v-btn large disabled>Disabled</v-btn>
+    </div>
+
+    <div>
+      <v-btn depressed small color="primary">Primary</v-btn>
+      <v-btn depressed color="error">Error</v-btn>
+      <v-btn depressed large>Normal</v-btn>
+      <v-btn depressed large disabled>Disabled</v-btn>
+    </div>
+
+    <div>
+      <span>button dropdown</span>
+      <v-container id="dropdown-example" grid-list-xl>
+        <v-layout row wrap>
+          <v-flex xs12 sm4>
+            <p>Overflow</p>
+
+            <v-overflow-btn
+              :items="dropdown_font"
+              label="Overflow Btn"
+              target="#dropdown-example"
+            ></v-overflow-btn>
+          </v-flex>
+
+          <v-flex xs12 sm4>
+            <p>Segmented</p>
+
+            <v-overflow-btn
+              :items="dropdown_icon"
+              label="Segmented Btn"
+              segmented
+              target="#dropdown-example"
+            ></v-overflow-btn>
+          </v-flex>
+
+          <v-flex xs12 sm4>
+            <p>Editable</p>
+
+            <v-overflow-btn
+              :items="dropdown_edit"
+              label="Editable Btn"
+              editable
+              item-value="text"
+            ></v-overflow-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+
+      <v-card flat class="py-5">
+        <v-card-text>
+          <v-container fluid class="pa-0">
+            <v-layout row wrap>
+              <v-flex xs12 sm6 class="py-2">
+                <p>Exclusive</p>
+                <v-btn-toggle v-model="toggle_exclusive">
+                  <v-btn flat>
+                    <v-icon>format_align_left</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_center</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_right</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_justify</v-icon>
+                  </v-btn>
+                </v-btn-toggle>
+              </v-flex>
+              <v-flex xs12 sm6 class="py-2">
+                <p>Multiple</p>
+                <v-btn-toggle v-model="toggle_multiple" multiple>
+                  <v-btn flat>
+                    <v-icon>format_bold</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_italic</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_underlined</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_color_fill</v-icon>
+                  </v-btn>
+                </v-btn-toggle>
+              </v-flex>
+              <v-flex xs12 sm6 class="py-2">
+                <p>No Options Selected</p>
+                <v-btn-toggle v-model="toggle_none">
+                  <v-btn flat>
+                    <v-icon>format_align_left</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_center</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_right</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_justify</v-icon>
+                  </v-btn>
+                </v-btn-toggle>
+              </v-flex>
+              <v-flex xs12 sm6 class="py-2">
+                <p>Mandatory</p>
+                <v-btn-toggle v-model="toggle_one" mandatory>
+                  <v-btn flat>
+                    <v-icon>format_align_left</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_center</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_right</v-icon>
+                  </v-btn>
+                  <v-btn flat>
+                    <v-icon>format_align_justify</v-icon>
+                  </v-btn>
+                </v-btn-toggle>
+              </v-flex>
+              <v-flex xs12 sm6 class="py-2">
+                <p>Text Options</p>
+                <v-btn-toggle v-model="text">
+                  <v-btn flat value="left">
+                    Left
+                  </v-btn>
+                  <v-btn flat value="center">
+                    Center
+                  </v-btn>
+                  <v-btn flat value="right">
+                    Right
+                  </v-btn>
+                  <v-btn flat value="justify">
+                    Justify
+                  </v-btn>
+                </v-btn-toggle>
+              </v-flex>
+              <v-flex xs12 sm6 class="py-2">
+                <p>Text &amp; Icon Options</p>
+                <v-btn-toggle v-model="icon">
+                  <v-btn flat value="left">
+                    <span>Left</span>
+                    <v-icon>format_align_left</v-icon>
+                  </v-btn>
+                  <v-btn flat value="center">
+                    <span>Center</span>
+                    <v-icon>format_align_center</v-icon>
+                  </v-btn>
+                  <v-btn flat value="right">
+                    <span>Right</span>
+                    <v-icon>format_align_right</v-icon>
+                  </v-btn>
+                  <v-btn flat value="justify">
+                    <span>Justify</span>
+                    <v-icon>format_align_justify</v-icon>
+                  </v-btn>
+                </v-btn-toggle>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </div>
+
+    <div>
+      <v-btn flat icon color="pink">
+        <v-icon>favorite</v-icon>
+      </v-btn>
+      <v-btn flat icon color="indigo">
+        <v-icon>star</v-icon>
+      </v-btn>
+      <v-btn flat icon color="green">
+        <v-icon>cached</v-icon>
+      </v-btn>
+      <v-btn flat icon color="deep-orange">
+        <v-icon>thumb_up</v-icon>
+      </v-btn>
+      <v-btn icon disabled>
+        <v-icon>thumb_up</v-icon>
+      </v-btn>
+    </div>
+
+    <div>
+      <v-btn fab dark small color="primary">
+        <v-icon dark>remove</v-icon>
+      </v-btn>
+      <v-btn fab dark color="teal">
+        <v-icon dark>list</v-icon>
+      </v-btn>
+      <v-btn fab dark large color="purple">
+        <v-icon dark>android</v-icon>
+      </v-btn>
+    </div>
+
+    <div>
+      <v-btn
+        :loading="loading3"
+        :disabled="loading3"
+        color="blue-grey"
+        class="white--text"
+        @click.native="loader = 'loading3'"
+      >
+        Upload
+        <v-icon right dark>cloud_upload</v-icon>
+      </v-btn>
+
+      <v-btn
+        :loading="loading2"
+        :disabled="loading2"
+        color="success"
+        @click.native="loader = 'loading2'"
+      >
+        Custom Loader
+        <span slot="loader">Loading...</span>
+      </v-btn>
+
+      <v-btn
+        :loading="loading4"
+        :disabled="loading4"
+        color="info"
+        @click.native="loader = 'loading4'"
+      >
+        Icon Loader
+        <span slot="loader" class="custom-loader">
+          <v-icon light>cached</v-icon>
+        </span>
+      </v-btn>
+    </div>
+
+    <div>
+      <v-btn outline color="indigo">Outline Button</v-btn>
+      <v-btn outline fab color="teal">
+        <v-icon>list</v-icon>
+      </v-btn>
+      <v-btn outline large fab color="indigo">
+        <v-icon>edit</v-icon>
+      </v-btn>
+    </div>
+
+    <div>
+      <v-btn round color="primary" dark>Rounded Button</v-btn>
+      <v-btn block color="secondary" dark>Block Button</v-btn>
+    </div>
   </v-flex>
 
   </div>
@@ -477,7 +725,44 @@ export default {
         { text: 'Dashboard', disabled: false },
         { text: 'Link 1', disabled: false },
         { text: 'Link 2', disabled: true }
-      ]
+      ],
+      //button dropdown
+      dropdown_font: ['Arial', 'Calibri', 'Courier', 'Verdana'],
+      dropdown_icon: [
+        { text: 'list', callback: () => console.log('list') },
+        { text: 'favorite', callback: () => console.log('favorite') },
+        { text: 'delete', callback: () => console.log('delete') }
+      ],
+      dropdown_edit: [
+        { text: '100%' },
+        { text: '75%' },
+        { text: '50%' },
+        { text: '25%' },
+        { text: '0%' }
+      ],
+      // toggle button
+      text: 'center',
+      icon: 'justify',
+      toggle_none: null,
+      toggle_one: 0,
+      toggle_exclusive: 2,
+      toggle_multiple: [0, 1, 2],
+      // loaders
+      loader: null,
+      loading: false,
+      loading2: false,
+      loading3: false,
+      loading4: false
+    }
+  },
+  watch: {
+    loader () {
+      const l = this.loader
+      this[l] = !this[l]
+
+      setTimeout(() => (this[l] = false), 3000)
+
+      this.loader = null
     }
   },
   computed: {
@@ -513,4 +798,42 @@ li {
 a {
   color: #42b983;
 }
+
+/* loaders */
+.custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 </style>
